df6e20dc45bf180fd19891d0d07dd78183a4c152
imSaurav06 <sauravkumaronly001@gmail.com@email.com>
2026-06-28 23:20:34 +0530

    .

--- Lecture 4 - First Node Server/practice2.js
+++ Lecture 4 - First Node Server/practice2.js
@@ -8,25 +8,30 @@ const server = http.createServer((req,res)=>{
 console.log(req.url,req.method);
 
 if (req.url==='/home') {
-  res.write(`<h>welcome to the home</h>`)
+  res.write(`<h>welcome to the home</h>`);
+  return res.end();
 }
 else if(req.url==='/men'){
- res.write(`<h>welcome to the men</h>`)
+ res.write(`<h>welcome to the men</h>`);
+ return res.end();
 }
 else if(req.url==='/women'){
- res.write(`<h>welcome to the women</h>`)
+ res.write(`<h>welcome to the women</h>`);
+ return res.end();
 }
 
 else if(req.url==='/kid'){
- res.write(`<h>welcome to the kid</h>`)
+ res.write(`<h>welcome to the kid</h>`);
+ return res.end();
 }
 else if(req.url==='/cart'){
- res.write(`<h>welcome to the Cart</h>`)
+ res.write(`<h>welcome to the Cart</h>`);
+ return res.end();
 }
 
 else
-res.write(
-  `<html>
+res.write(`
+  <html>
     <head>
      <tital>MYNTRA</tital>
     </head>
@@ -35,17 +40,18 @@ res.write(
     <head>
       <nav>
         <ul>
-          <li><a href="/home"></a>Home</li>
-          <li><a href="/men"></a>MEn</li>
-          <li><a href="/womwn"></a>women</li>
-          <li><a href="/homekid"></a>kid</li>
-          <li><a href="/cart"></a>cart</li>
+          <li><a href="/home">Home<</a>/li>
+          <li><a href="/men">MEn</a></li>
+          <li><a href="/womwn">women</a></li>
+          <li><a href="/homekid">kid</a></li>
+          <li><a href="/cart">cart</a></li>
         </ul>
       </nav>
     </head>
     </body>
   </html>`
-)
+);
+return res.end();
 });
 
 const PORT = 5003;
