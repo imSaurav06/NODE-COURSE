0fd26c516aa8fa4dee61eba832f710cb03d83451
imSaurav06 <sauravkumaronly001@gmail.com@email.com>
2026-06-28 16:44:15 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,12 @@
+console.log('kg coding can help get into jog or hiring')
+
+
+const  fs = require('fs')
+
+fs.writeFile("output.text","writing File" , (err)=>{
+  if(err){console.log("error occurred");}
+  else console.log('File wirting succsesfully')
+})
+
+
+// REPL(read , Evaluate, Print, Loop) in Terminal
