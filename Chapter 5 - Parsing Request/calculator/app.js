a65e4bde4a78a1bf97fb71b0bdece9f15d28738f
imSaurav06 <156805591+imSaurav06@users.noreply.github.com>
2026-06-28 22:05:53 +0000

    .

--- Chapter 5 - Parsing Request/calculator/app.js
+++ Chapter 5 - Parsing Request/calculator/app.js
@@ -1,1 +1,9 @@
-co
+const http = require('http');
+
+const handlerUI = require('./user')
+
+const server = http.createServer(handlerUI);
+
+server.listen(PORT, ()=>{
+    console.log(`Server running on address http://localhost:&{PORT}`);
+})
