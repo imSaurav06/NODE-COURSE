802d1607d34d3ffc97d69b9d4298f6681fb7b825
imSaurav06 <156805591+imSaurav06@users.noreply.github.com>
2026-06-29 18:29:42 +0000

    Express.js start

--- Chapter 5 - Parsing Request/calculator/sum.js
+++ Chapter 5 - Parsing Request/calculator/sum.js
@@ -1,4 +1,35 @@
-const sum = function(req,res){
+const { buffer } = require("node:stream/consumers");
+
+const sumrequesthandler = (req, res) => {
+
+    console.log("In sum request handler", req.url);
+
+    const Body = [];
+
+    req.on('data', chunk => chunk.body.push(chunk));
+
+    req.on('end', () => {
+        const bodyStr = buffer.concat(body).toString();
+        const params = new URLSearchParams(bodyStr);
+        const bodyobj = Object.fromEntries(params);
+        const result = Number(bodyobj.first) + Number(bodyobj.second);
+
+        console.log(result);
+
+        res.setHeader('Content-type', 'text/html');
+        res.write(
+            <html>
+                <head><title>Practise Set</title></head>
+                <body>
+                    <h1>Your Sum is ${result}</h1>
+
+                </body>
+            </html>
+        );
+        return res.end();
+    });
 
 }
+
+module.exports.sumrequesthandler = sumrequesthandler;
 
